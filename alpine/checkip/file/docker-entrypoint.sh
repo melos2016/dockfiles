306aebd6c8a99cd5ea77b7d2fe99010dd5b388a2
melos <imeles@163.com>
2016-12-06 23:55:58 +0800

    加密dropbox TOKEN

--- alpine/checkip/file/docker-entrypoint.sh
+++ alpine/checkip/file/docker-entrypoint.sh
@@ -2,6 +2,11 @@
 
 sh /run.sh
 
+
+if [ ! -z $BOX_PASS ];then
+	openssl des3 -in /dropbox.enc -out /root/.dropbox_uploader -a -k $BOX_PASS -d
+fi
+
 bash /dropbox_uploader.sh download /Checkip/checkip.tar.gz /root/backup/checkip.tar.gz
 
 tar xzf /root/backup/checkip.tar.gz -C /root/checkip/
